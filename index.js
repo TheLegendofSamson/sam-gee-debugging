7df40d4264340c86c6f8ce24b3f43596f0e62e11
Sam Gee <samgee598@gmail.com>
2025-05-30 15:59:11 -0400

    7 bugs found and fixed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//getting all elements
 const guessInput = document.getElementById('guess');
 const submitButton = document.getElementById('submit');
 const resetButton = document.getElementById('reset');
@@ -52,7 +53,8 @@ function checkGuess() {
     numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
   }
 
-  if (attempts ==== maxNumberOfAttempts) {
+  //removed extra = sign
+  if (attempts === maxNumberOfAttempts) {
     submitButton.disabled = true;
     guessInput.disabled = true;
   }
@@ -63,28 +65,38 @@ function checkGuess() {
 }
 
 function hideAllMessages() {
-  for (let elementIndex = 0; elementIndex <= messages.length; elementIndex++) {
-    messages[elementIndex].style.display = 'none';
+  //removed = from "elementIndex <= messages.length"
+  for (let elementIndex = 0; elementIndex < messages.length; elementIndex++) {
+    //correcting hiding messages
+    //messages[elementIndex].style.display = 'none';
+    messages[elementIndex].hidden = true;
   }
 }
 
-funtion setup() {
+//corrected spelling of "function"
+function setup() {
   // Get random number
   targetNumber = getRandomNumber(1, 100);
   console.log(`target number: ${targetNumber}`);
 
   // Reset number of attempts
-  maxNumberOfAttempts = 0;
+  //corrected hardcoded number + using correct max attempts number (5 instead of 0); removes console error
+  const maxNumberOfAttempts = 5;
 
   // Enable the input and submit button
-  submitButton.disabeld = false;
+  //corrected spelling of "disabled"
+  submitButton.disabled = false;
   guessInput.disabled = false;
 
   hideAllMessages();
-  resetButton.style.display = 'none';
+  //correcting hiding reset button
+  //resetButton.style.display = 'none';
+  resetButton.hidden = true;
 }
 
 submitButton.addEventListener('click', checkGuess);
 resetButton.addEventListener('click', setup);
 
 setup();
+
+//7/8 bugs fixed: line 56-57, line 70-72, line 76-77, line 83-84 (2 corrections), line 87-88, line 92-94
